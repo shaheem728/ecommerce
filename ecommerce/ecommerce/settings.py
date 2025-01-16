183dd35f09717118b902ddc16b40bb67194fe3ad
shaheem <shaheemmahmood728@gmail.com>
2025-01-17 04:27:57 +0530

    `Updated project files and settings for production deployment`

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cvsz_=gvz#p83$mvsc&6b*q0$=(wc%3m&tgp$4a%#$^%yu7flv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
